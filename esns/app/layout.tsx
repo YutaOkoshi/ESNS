694411f607cb6291d805ac806cf6975daee3e7e2
YuutaOokoshi <ookoshiyuuta@gmail.com>
2024-11-27 22:48:32 +0900

    npm i next-auth  firebase firebase-admin

--- esns/app/layout.tsx
+++ esns/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 import { AuthProvider } from './contexts/AuthContext'
+import SessionProvider from "./provider/SessionProvider";
 
 const inter = Inter({ subsets: ['latin'] })
 
