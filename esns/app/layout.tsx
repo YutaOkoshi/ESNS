28af9bb28042d35590633d161b4cf6a6c8ed3d8d
YuutaOokoshi <ookoshiyuuta@gmail.com>
2024-12-10 13:24:21 +0900

    fix no used

--- esns/app/layout.tsx
+++ esns/app/layout.tsx
@@ -1,7 +1,7 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 import { AuthProvider } from './contexts/AuthContext'
-import SessionProvider from "./provider/SessionProvider";
+// import SessionProvider from "./provider/SessionProvider";
 
 const inter = Inter({ subsets: ['latin'] })
 
